5098779d93417285f1c5336768156eff3794842e
Ludvig Dickman <ludvig@avidity.se>
2014-01-29 14:01:13 +0100

    Moved tracked_delay_job specs to own file

---
+++ spec/tracked/delayed_job/tracked_delay_proxy_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Tracked::DelayedJob::Plugin do
+
+  before do
+    mock_model('MockModel')
+    MockModel.any_instance.stub(:calculate) { "result" }
+  end
+
+  let(:stub_object) { MockModel.new }
+
+  it "should create a tracked_job when calling tracked_delay" do
+    expect {
+      stub_object.tracked_delay.calculate
+    }.to change { Tracked::Job.count }.from(0).to(1)
+  end
+
+  it "should return the created tracked_job uuid" do
+    SecureRandom.stub(:uuid) { "uuid" }
+
+    expect(stub_object.tracked_delay.calculate).to eq "uuid"
+  end
+end
