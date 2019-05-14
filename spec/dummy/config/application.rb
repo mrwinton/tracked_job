1382bbad153c73d544a782c900cc2a79ac212b73
Michael Winton <wintonmr@gmail.com>
2019-05-14 11:52:26 +0200

    Fix deprecation issue in specs for SQLite
    Setting the config to use the `represent_boolean_as_integer` flag
    resolves the issue:
    
    ```
    DEPRECATION WARNING: Leaving `ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer`
    set to false is deprecated. SQLite databases have used 't' and 'f' to serialize
    boolean values and must have old data converted to 1 and 0 (its native boolean
    serialization) before setting this flag to true. Conversion can be accomplished
    by setting up a rake task which runs
    
      ExampleModel.where("boolean_column = 't'").update_all(boolean_column: 1)
      ExampleModel.where("boolean_column = 'f'").update_all(boolean_column: 0)
    
    for all models and all boolean columns, after which the flag must be set to
    true by adding the following to your application.rb file:
    
      Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
    ```

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -25,6 +25,8 @@ module Dummy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.active_record.sqlite3.represent_boolean_as_integer = true
+
     Delayed::Worker.plugins << Tracked::DelayedJob::Plugin
     Delayed::Worker.backend = :active_record
   end
